d1b777c8d46e8b35edd7125d4097fe22cd1fc05e
PhilipeFernandino <57276004+PhilipeFernandino@users.noreply.github.com>
2022-02-05 18:15:28 -0300

    Adicionando comentários explicando o comportamento do script

--- Assets/Scripts/LightEmitter.cs
+++ Assets/Scripts/LightEmitter.cs
@@ -1,21 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+//A maioria dos scripts serão derivados do MonoBehaviour, que é a classe base para todos os objetos do jogo (gameObjects)
 public class LightEmitter : MonoBehaviour {
 
+    //Variáveis públicas podem ser acessadas por outras classes e também podem ser editadas pelo Inspector da engine
     public GameObject LightParticlePrefab;
     public float emitterPower = 10f;
 
     //Update() é uma função que a engine chama todo frame
     private void Update() {
+        //Se for detectado um clique com o botão direito
         if (Input.GetMouseButtonDown(0)) {
             EmitLightParticle();
         }
     }
 
     private void EmitLightParticle() {
+        
+        //Instanciando um prefab (pense como uma classe, mas que existe no jogo) usando a 
+        //posição do objeto (transform.position --> é derivada do monobehaviour) ao qual esse script (LightEmitter) está acoplado
         GameObject lightParticle = Instantiate(LightParticlePrefab, transform.position, Quaternion.identity);
-        lightParticle.GetComponent<Rigidbody2D>().velocity = Vector2.up * emitterPower;
+        
+        //Usando a referência do prefab, pegamos o Rigidbody2D (que é apenas um script que está acoplado ao prefab) e definimos sua
+        //velocidade, usando a rotação do LightEmitter (transform.up (um vetor que aponta para cima do lightEmitter)) e multiplicamos
+        //pela força que podemos definir no editor ou no script
+        lightParticle.GetComponent<Rigidbody2D>().velocity = transform.up * emitterPower;
     }
 }
