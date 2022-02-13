17715969a6e5a8c267133e6c153c0b5a6b3a05f1
PhilipeFernandino <57276004+PhilipeFernandino@users.noreply.github.com>
2022-02-13 17:45:11 -0300

    Adicionando cena de teste para UI, Text Mesh Pro (da engine), e controlador da ui

---
+++ Assets/Scripts/UI/GameController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public int availableReflectors;
+    public GameObject reflectorPrefab;
+
+    private GameObject draggingReflector; 
+    private bool isDragging;
+
+    public void PlaceReflector() {
+        availableReflectors--;
+        isDragging = true;
+        draggingReflector = Instantiate(reflectorPrefab, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
+    }
+    
+    public void Update() {
+        if (Input.GetMouseButtonDown(0) && isDragging) isDragging = false;
+        if (isDragging) {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = 0;
+            draggingReflector.transform.position = pos;
+        }
+    }
+}
