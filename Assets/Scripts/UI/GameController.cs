99f29e18e7dfba5dabd488faa29202f4790e1c20
PhilipeFernandino <57276004+PhilipeFernandino@users.noreply.github.com>
2022-02-22 10:40:50 -0300

    Adicionando reset

--- Assets/Scripts/UI/GameController.cs
+++ Assets/Scripts/UI/GameController.cs
@@ -1,14 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
-    public int availableReflectors;
+    public int reflectors;
     public GameObject reflectorPrefab;
 
     private GameObject draggingReflector; 
     private bool isDragging;
+    private int availableReflectors;
     
+    private void Start() {
+        availableReflectors = reflectors;
+    }
+
     public void PlaceReflector() {
         if (availableReflectors > 0) {
             availableReflectors--;
@@ -16,9 +19,23 @@ public class GameController : MonoBehaviour {
             draggingReflector = Instantiate(reflectorPrefab, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
         }
     }
+
+    private void Reset() {
+        GameObject[] GOs = GameObject.FindGameObjectsWithTag("LightReflector");
+        foreach(GameObject go in GOs) {
+            Destroy(go);
+        }
+        availableReflectors = reflectors;
+    }
     
     public void Update() {
+        
+        if (Input.GetKeyDown(KeyCode.R)) {
+            Reset();
+        }
+        
         if (Input.GetMouseButtonDown(0) && isDragging) isDragging = false;
+        
         if (isDragging) {
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             pos.z = 0;
