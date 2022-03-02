6cb7887992df47812ef7d741f5ee0bcdbeea4b30
PhilipeFernandino <57276004+PhilipeFernandino@users.noreply.github.com>
2022-03-02 00:30:27 -0300

    Feat: click em objetos para realocá-los (e permitir a rotação :thumbs_up:)

--- Assets/Scripts/UI/GameController.cs
+++ Assets/Scripts/UI/GameController.cs
@@ -3,23 +3,12 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
     public int reflectors;
     public GameObject reflectorPrefab;
-
-    private GameObject draggingReflector; 
-    private bool isDragging;
+    
+    private bool isObjectDraggable;
+    private bool isObjectSelected;
+    private GameObject selectedObject; 
     private int availableReflectors;
     
-    private void Start() {
-        availableReflectors = reflectors;
-    }
-
-    public void PlaceReflector() {
-        if (availableReflectors > 0) {
-            availableReflectors--;
-            isDragging = true;
-            draggingReflector = Instantiate(reflectorPrefab, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
-        }
-    }
-
     private void Reset() {
         GameObject[] GOs = GameObject.FindGameObjectsWithTag("LightReflector");
         foreach(GameObject go in GOs) {
@@ -27,19 +16,57 @@ public class GameController : MonoBehaviour {
         }
         availableReflectors = reflectors;
     }
+
+    public void PlaceReflector() {
+        if (availableReflectors > 0) {
+            availableReflectors--;
+            isObjectSelected = true;
+            isObjectDraggable = true;
+            selectedObject = Instantiate(reflectorPrefab, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
+        }
+    }
     
+    private void Start() {
+        availableReflectors = reflectors;
+    }
+
     public void Update() {
-        
+    
         if (Input.GetKeyDown(KeyCode.R)) {
             Reset();
         }
+
+        if (Input.GetMouseButtonDown(0)) {
+            if (isObjectSelected) isObjectSelected = false;
+            else {
+                Vector2 mouseOnWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                RaycastHit2D hit = Physics2D.Raycast(mouseOnWorldPos, Vector2.zero);
+                
+                if (hit.collider != null) {
+                    
+                    GameObject GO = hit.collider.gameObject;
+                    //talvez fosse melhor se cada objeto decidisse como ele deseja se mover, chamando um metodo de interface para 
+                    //o objeto selecionado 
+                    if (GO.CompareTag("LightReflector")) {
+                        selectedObject = hit.collider.transform.parent.gameObject;
+                        isObjectDraggable = true;
+                        isObjectSelected = true;
+                    } 
+                    
+                    else if (GO.CompareTag("LightEmitter")) {
+                        selectedObject = hit.collider.gameObject;
+                        isObjectDraggable = false;
+                        isObjectSelected = true;
+                    }
+
+                }
+            }
+        }
         
-        if (Input.GetMouseButtonDown(0) && isDragging) isDragging = false;
-        
-        if (isDragging) {
+        if (isObjectSelected && isObjectDraggable) {
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             pos.z = 0;
-            draggingReflector.transform.position = pos;
+            selectedObject.transform.position = pos;
         }
     }
 }
