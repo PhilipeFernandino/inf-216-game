7f795ff956c116e4837915c32d6ef85de86b8ade
PhilipeFernandino <57276004+PhilipeFernandino@users.noreply.github.com>
2022-03-03 15:46:01 -0300

    Update: refletor só tem 1 colisor e reflete em todas as direções

--- Assets/Scripts/LightParticle.cs
+++ Assets/Scripts/LightParticle.cs
@@ -23,8 +23,8 @@ public class LightParticle : MonoBehaviour {
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
+       
         if (justCollided || !rb.isKinematic) return;
-
         Debug.Log("Colidiu com: " + other.gameObject.tag);
         
         GameObject otherGO = other.gameObject;
@@ -33,13 +33,25 @@ public class LightParticle : MonoBehaviour {
             Destroy(gameObject);
             return;
         }
+    }
 
+    private void OnCollisionEnter2D(Collision2D other) {
+        
+        if (justCollided || !rb.isKinematic) return;
+        Debug.Log("Colidiu com: " + other.gameObject.tag);
+        
+        foreach (var contact in other.contacts) {
+            Debug.DrawRay(contact.point, contact.normal, Color.red, 2f);
+            Debug.Log("Hit normal: " + contact.normal);
+        }
+
+        GameObject otherGO = other.gameObject;
         if (otherGO.CompareTag("LightReflector")) {
             justCollided = true;
             timerSinceLastCollision = 0f;
 
             Collider2D lightReflectorCollider = otherGO.GetComponent<Collider2D>();
-            Vector2 dir = Vector2.Reflect(rb.velocity, lightReflectorCollider.transform.up).normalized;
+            Vector2 dir = Vector2.Reflect(rb.velocity, other.GetContact(0).normal).normalized;
 
             rb.velocity = dir * keepVelocity;
             return;
